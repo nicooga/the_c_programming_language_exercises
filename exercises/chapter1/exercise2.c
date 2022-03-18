212f45a39ae0c9723663c63e508353bc00ae3a4e
Nicolas Oga <2112.oga@gmail.com>
2022-03-18 12:42:15 -0300

    Modify Ex. 1.2

--- exercises/chapter1/exercise2.c
+++ exercises/chapter1/exercise2.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 
+// Exercise 1-2. Experiment to find out what happens when prints's argument string contains
+// \c, where c is some character not listed above.
 int main(void)
 {
     printf("\d");
