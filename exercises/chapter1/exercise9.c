7b13e9b2fc6a4a5209f163f0b27767f20927f27a
Nicolas Oga <2112.oga@gmail.com>
2022-03-18 14:20:31 -0300

    Ex. 1.9

---
+++ exercises/chapter1/exercise9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define NORMAL 0
+#define CONSUMING_BLANKS 1
+
+// Exercise 1-9. Write a program to copy its input to its output,
+//  replacing each string of one or more blanks by a single blank.
+int main(void)
+{
+    int state = NORMAL;
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            if (state == NORMAL) {
+                putchar(' ');
+                state = CONSUMING_BLANKS;
+            }
+        } else {
+            state = NORMAL;
+            putchar(c);
+        }
+    }
+}
