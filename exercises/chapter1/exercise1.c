db268d515c9e64623415d81cb1691f098b0aece3
Nicolas Oga <2112.oga@gmail.com>
2022-03-18 12:26:44 -0300

    Exercise 1.1

---
+++ exercises/chapter1/exercise1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// Exercise 1-1. Run the ``hello, world'' program on your system. Experiment with leaving
+// out parts of the program, to see what error messages you get.
+int main(void)
+{
+    printf("hello, world\n");
+}
