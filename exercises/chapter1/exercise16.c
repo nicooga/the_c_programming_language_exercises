8ee2473bb254a0710e42d19b53ea08ddc2a30893
Nicolas Oga <2112.oga@gmail.com>
2022-03-21 20:35:35 -0300

    Ex. 1.16

---
+++ exercises/chapter1/exercise16.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define MAX_LENGTH 12
+
+int consume_next_line(char line[], int max_length); // Avoid name collision with already defined `get_line` function
+void copy(char from[], char to[], int lim);
+
+// Exercise 1-16. Revise the main routine of the longest-line program so it will correctly print
+// the length of arbitrary long input lines, and as much as possible of the text.
+int main()
+{
+    int length;
+    int max_length;
+    char line[MAX_LENGTH];
+    char longest_line[MAX_LENGTH];
+
+    while ((length = consume_next_line(line, MAX_LENGTH)) > 0)
+        if (length > max_length) {
+            max_length = length;
+            copy(line, longest_line, MAX_LENGTH);
+        }
+
+    if (max_length > 0)
+        printf("%s\n", longest_line);
+
+    return 0;
+}
+
+int consume_next_line(char s[], int max_length)
+{
+    int i, c;
+
+    for (i = 0; (c = getchar()) != EOF; ++i) {
+        if (i < max_length) s[i] = c;
+        if (c == '\n') break;
+    }
+
+    return i;
+}
+
+void copy(char from[], char to[], int lim)
+{
+    for (int i = 0; i < lim && (to[i] = from[i]) != '\0'; ++i);
+}
