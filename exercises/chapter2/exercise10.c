779e5396895ac961484e47be81d0fcf95b8e8cb0
Nicolas Oga <2112.oga@gmail.com>
2022-03-28 20:06:19 -0300

    Ex. 2.10

---
+++ exercises/chapter2/exercise10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define MAX_LENGTH 1000
+
+int lower(int c);
+
+// Exercise 2-10. Rewrite the function lower, which converts upper case letters to lower case,
+// with a conditional expression instead of if-else. 
+int main(void)
+{
+    char c = getchar();
+    printf("%c\n", lower(c));
+    return 0;
+}
+
+int lower(int c)
+{
+    return c >= 'A' && c <= 'Z' ? c + 'a' - 'A' : c;
+}
