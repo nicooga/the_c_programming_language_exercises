42fbcff9510a5f5e493130d9fc9e6333dc70af8f
Nicolas Oga <2112.oga@gmail.com>
2022-04-15 19:15:32 -0300

    Add reverse functions to string module

--- lib/string/string.c
+++ lib/string/string.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int string_equals(char s1[], char s2[]);
+
+void string_reverse(char s[]);
+void string_reverse_up_to(char s[], int p);
+void _string_reverse(char s[], int p);
 
 int string_equals(char s1[], char s2[])
 {
@@ -8,4 +13,24 @@ int string_equals(char s1[], char s2[])
         if (s1[i] != s2[i]) return 0;
 
     return 1;
+}
+
+void string_reverse(char s[])
+{
+    int l = strlen(s);
+    _string_reverse(s, l);
+}
+
+void string_reverse_up_to(char s[], int p)
+{
+    _string_reverse(s, p+1);
+}
+
+void _string_reverse(char s[], int p)
+{
+    for (int i = 0; i < p/2; i++) {
+        char temp = s[i];
+        s[i] = s[p-1-i];
+        s[p-1-i] = temp;
+    }
 }
--- lib/string/test.c
+++ lib/string/test.c
@@ -7,5 +7,13 @@ int main(void)
     if (string_equals("qasdf", "asdf") != 0) return 1;
     if (string_equals("asd", "asf") != 0) return 1;
 
+    char s[9] = "asdfqwer";
+
+    string_reverse(s);
+    if (string_equals(s, "rewqfdsa") != 1) return 1;
+
+    string_reverse_up_to(s, 3);
+    if (string_equals(s, "qwerfdsa") != 1) return 1;
+
     return 0;
 }
