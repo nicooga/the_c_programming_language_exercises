d2ed97c18e4190f73d878de71813f697c002d963
Nicolas Oga <2112.oga@gmail.com>
2022-04-17 20:35:44 -0300

    Ex. 5.3

---
+++ exercises/chapter5/exercise3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void my_strcat(char *s, char *t);
+
+// Exercise 5-3. Write a pointer version of the function strcat that we showed in Chapter 2:
+// strcat(s,t) copies the string t to the end of s.
+int main(void)
+{
+    char s1[] = "asdf";
+    char s2[] = "qwer";
+
+    my_strcat(s1, s2);
+
+    if (strcmp(s1, "asdfqwer") != 0) return 1;
+
+    return 0;
+}
+
+void my_strcat(char *s, char *t)
+{
+    while (*s != '\0') s++;
+    while (*s++ = *t++);
+}
--- exercises/chapter5/test.sh
+++ exercises/chapter5/test.sh
@@ -8,4 +8,6 @@ assert "compile_and_run_ex 5 1" "123\n456\n" " 123 - 456 "
 assert "compile_and_run_ex 5 2" "12.300000\n4.560000\n" " 12.3   4.56   "
 assert "compile_and_run_ex 5 2" "12.300000\n4.560000\n" " 12.3 - 4.56 "
 
+assert_raises "compile_and_run_ex 5 3" 0
+
 assert_end "Chapter 5"
