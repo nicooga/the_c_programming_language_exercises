da151c955c997aa9ce51d041c8a317d91190cf26
Nicolas Oga <2112.oga@gmail.com>
2022-04-24 18:41:57 -0300

    Ex. 5.9

---
+++ exercises/chapter5/exercise9.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include "input/input.c"
+#include "string/string.c"
+
+int invoke_day_of_year(void);
+int invoke_month_day(void);
+int invoke_error(char function_name[]);
+
+int day_of_year(unsigned short year, char month, char day);
+void month_day(
+    short year,
+    short year_day,
+    short *pmonth,
+    short *pday
+);
+
+static char is_leap(unsigned short year);
+static int validate_year(short year);
+static int validate_month(short month);
+static int validate_day(short day);
+static int validate_year_day(short year, short year_day);
+
+static char DAYTABS[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+// Exercise 5-9. Rewrite the routines day_of_year and month_day with pointers
+// instead of indexing.
+int main(void)
+{
+    char function_name[100];
+
+    read_word(function_name, 100);
+
+    if (string_equals(function_name, "day_of_year")) return invoke_day_of_year();
+    else if (string_equals(function_name, "month_day")) return invoke_month_day();
+    else return invoke_error(function_name);
+
+    return 0;
+}
+
+int invoke_day_of_year(void)
+{
+    short year = read_int();
+    short month = read_int();
+    short day = read_int();
+    if (!validate_year(year)) return 1;
+    if (!validate_month(month)) return 1;
+    if (!validate_day(day)) return 1;
+    printf("%d\n", day_of_year(year, month, day));
+    return 0;
+}
+
+int invoke_month_day(void)
+{
+    short year = read_int();
+    short year_day = read_int();
+    short month;
+    short day;
+    if (!validate_year(year)) return 1;
+    if (!validate_year_day(year, year_day)) return 1;
+    month_day(year, year_day, &month, &day);
+    printf("%d %d\n", month, day);
+    return 0;
+}
+
+int invoke_error(char function_name[])
+{
+    printf("Unknown function \"%s\"\n", function_name);
+    return 1;
+}
+
+int validate_year(short year)
+{
+    if (year > 0) return 1;
+    printf("Invalid year `%d`. Year must be a positive number\n", year);
+    return 0;
+}
+
+int validate_month(short month)
+{
+    if (month < 1) {
+        printf("Invalid month `%d`. Month must be a positive number\n", month);
+        return 0;
+    }
+
+    if (month > 12) {
+        printf("Invalid month `%d`. Month must be a less than or equal to 12\n", month);
+        return 0;
+    }
+
+    return 1;
+}
+
+int validate_day(short day)
+{
+    if (day < 1) {
+        printf("Invalid day `%d`. Day must be a positive number\n", day);
+        return 0;
+    }
+
+    if (day > 31) {
+        printf("Invalid day `%d`. No day can be greater than 31\n", day);
+        return 0;
+    }
+
+    return 1;
+}
+
+int validate_year_day(short year, short year_day)  {
+    if (year_day < 1) {
+        printf("Invalid year day `%d`. Year day must be a positive number\n", year_day);
+        return 0;
+    }
+
+    int leap = is_leap(year);
+    int days_in_year = leap ? 366 : 365;
+
+    if (year_day > days_in_year) {
+        printf(
+            "Invalid year day `%d`. Year %d is%s a leap year and has only %d days\n",
+            year_day,
+            year,
+            leap ? "" : " not",
+            days_in_year
+        );
+        return 0;
+    }
+
+    return 1;
+}
+
+int day_of_year(unsigned short year, char month, char day)
+{
+    int i;
+    int leap = is_leap(year);
+
+    for (i = 1; i < month; i++) {
+        char *selected_daytab = *(DAYTABS + leap);
+        day += *(selected_daytab + i);
+    }
+
+    return day;
+}
+
+void month_day(
+    short year,
+    short year_day,
+    short *pmonth,
+    short *pday
+)
+{
+    int i;
+    int leap = is_leap(year);
+
+    for (i = 1; year_day > DAYTABS[leap][i]; i++) {
+        char *selected_daytab = *(DAYTABS + leap);
+        year_day -= *(selected_daytab + i);
+    }
+
+    *pmonth = i;
+    *pday = year_day;
+}
+
+char is_leap(unsigned short year)
+{
+    return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+}
--- exercises/chapter5/test.sh
+++ exercises/chapter5/test.sh
@@ -42,21 +42,28 @@ qw
 EOF
 )"
 
-assert "compile_and_run_ex 5 8" "113" "day_of_year  2022 04 23"
-assert "compile_and_run_ex 5 8" "Invalid year \`-1\`. Year must be a positive number\n" "day_of_year -1 04 23"
-assert "compile_and_run_ex 5 8" "Invalid year \`0\`. Year must be a positive number\n" "day_of_year 0 04 23"
-assert "compile_and_run_ex 5 8" "Invalid month \`-1\`. Month must be a positive number\n" "day_of_year 2002 -1 23"
-assert "compile_and_run_ex 5 8" "Invalid month \`0\`. Month must be a positive number\n" "day_of_year 2002 0 23"
-assert "compile_and_run_ex 5 8" "Invalid month \`13\`. Month must be a less than or equal to 12\n" "day_of_year 2002 13 23"
-assert "compile_and_run_ex 5 8" "Invalid day \`-1\`. Day must be a positive number\n" "day_of_year 2002 04 -1"
-assert "compile_and_run_ex 5 8" "Invalid day \`32\`. No day can be greater than 31\n" "day_of_year 2002 04 32"
+test_day_functions_behavior() {
+  cmd="compile_and_run_ex 5 $1"
 
-assert "compile_and_run_ex 5 8" "4 23" "month_day 2022 113"
-assert "compile_and_run_ex 5 8" "Invalid year \`-1\`. Year must be a positive number\n" "month_day -1 113"
-assert "compile_and_run_ex 5 8" "Invalid year \`0\`. Year must be a positive number\n" "month_day 0 113"
-assert "compile_and_run_ex 5 8" "Invalid year day \`-1\`. Year day must be a positive number\n" "month_day 1 -1"
-assert "compile_and_run_ex 5 8" "Invalid year day \`0\`. Year day must be a positive number\n" "month_day 1 0"
-assert "compile_and_run_ex 5 8" "Invalid year day \`366\`. Year 1 is not a leap year and has only 365 days\n" "month_day 1 366"
-assert "compile_and_run_ex 5 8" "Invalid year day \`367\`. Year 4 is a leap year and has only 366 days\n" "month_day 4 367"
+  assert "$cmd" "113" "day_of_year  2022 04 23"
+  assert "$cmd" "Invalid year \`-1\`. Year must be a positive number\n" "day_of_year -1 04 23"
+  assert "$cmd" "Invalid year \`0\`. Year must be a positive number\n" "day_of_year 0 04 23"
+  assert "$cmd" "Invalid month \`-1\`. Month must be a positive number\n" "day_of_year 2002 -1 23"
+  assert "$cmd" "Invalid month \`0\`. Month must be a positive number\n" "day_of_year 2002 0 23"
+  assert "$cmd" "Invalid month \`13\`. Month must be a less than or equal to 12\n" "day_of_year 2002 13 23"
+  assert "$cmd" "Invalid day \`-1\`. Day must be a positive number\n" "day_of_year 2002 04 -1"
+  assert "$cmd" "Invalid day \`32\`. No day can be greater than 31\n" "day_of_year 2002 04 32"
+
+  assert "$cmd" "4 23" "month_day 2022 113"
+  assert "$cmd" "Invalid year \`-1\`. Year must be a positive number\n" "month_day -1 113"
+  assert "$cmd" "Invalid year \`0\`. Year must be a positive number\n" "month_day 0 113"
+  assert "$cmd" "Invalid year day \`-1\`. Year day must be a positive number\n" "month_day 1 -1"
+  assert "$cmd" "Invalid year day \`0\`. Year day must be a positive number\n" "month_day 1 0"
+  assert "$cmd" "Invalid year day \`366\`. Year 1 is not a leap year and has only 365 days\n" "month_day 1 366"
+  assert "$cmd" "Invalid year day \`367\`. Year 4 is a leap year and has only 366 days\n" "month_day 4 367"
+}
+
+test_day_functions_behavior 8
+test_day_functions_behavior 9
 
 assert_end "Chapter 5"
