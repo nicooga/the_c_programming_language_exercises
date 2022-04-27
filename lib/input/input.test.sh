dc177c8a38abdb61af0cf07a73bf7bad7ffc219f
Nicolas Oga <2112.oga@gmail.com>
2022-04-26 21:37:34 -0300

    Add more hapyness

--- lib/input/input.test.sh
+++ lib/input/input.test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 compile "$(current_directory $0)/input.test.c" &&
 (echo "asdf" | ./a.out) &&
-echo "Input tests passed" || (echo "Input tests failed" && exit 1)
+echo "--- Input tests passed" || (echo "Input tests failed" && exit 1)
--- lib/string/string.test.sh
+++ lib/string/string.test.sh
@@ -1,3 +1,3 @@
 compile "$(current_directory $0)/string.test.c" &&
 ./a.out &&
-echo "String tests passed"
+echo "--- String tests passed"
